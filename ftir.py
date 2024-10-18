fd5508cc56906b8379940f1cc55f7b38bfced983
Theo Hannotte <tn.hannotte@nanogune.eu>
2024-10-18 11:27:45 +0200

    load hyperspectral images

--- ftir.py
+++ ftir.py
@@ -30,6 +30,50 @@ def loadSpectra(filename, harm):
     return wavenumber, amp * np.exp(1j*phase)
 
 def loadInterferograms(filename, harm):
+    pos, opticSigArr = loadInterferograms2D(filename, harm)
+    return pos[0,0,:,:], opticSigArr[0,0,:,:]
+
+def interArrayToSpectra(posIn,opticSig,**kwargs):
+    pos2D = np.reshape(posIn, (1,1,len(posIn),len(posIn[0])))
+    opticSig2D = np.reshape(opticSig, (1,1,len(opticSig),len(opticSig[0])))
+    wavenumber, spec = interArrayToSpectra2D(pos2D, opticSig2D, **kwargs)
+    return wavenumber, spec[0,0]
+
+
+def loadSpectraFromInter(filename, harm, **kwargs):
+    pos, opticSig = loadInterferograms(filename, harm)
+    return interArrayToSpectra(pos,opticSig, **kwargs)
+
+
+
+
+def interArrayToSpectra2D(posIn,opticSig,discardPhase = True, discardDC = True, windowF = None, paddingFactor = 1, shiftMaxToZero = False):
+    pos = posIn[0,0,0,:]
+    step = pos[1]-pos[0]
+    av = np.mean(opticSig,2)
+    nbRow = len(av)
+    nbCol = len(av[0])
+    if discardPhase:
+        av = np.transpose(np.transpose(av) * np.transpose(np.exp(-j*(np.angle(np.mean(av,2))))))
+    if discardDC:
+        av = np.transpose( np.transpose(av) - np.transpose(np.mean(av,2)))
+    if windowF != None:
+        window = windowF(len(av[0,0]))
+    else:
+        window = 1
+    apodized = av*window
+    apodized = np.concatenate((apodized,np.zeros((nbRow, nbCol, len(apodized)*(paddingFactor-1)))),2)
+    if shiftMaxToZero:
+        apodized = np.roll(apodized, -np.argmax(np.abs(apodized)),2)
+    if discardPhase:
+        wavenumber = np.linspace(0,0.25e-2/step,(int(len(apodized[0,0])/2))+1)
+        spectrum = np.fft.rfft(np.real(apodized))
+    else:
+        wavenumber = np.linspace(0,0.5e-2/step,len(apodized[0,0]))
+        spectrum = np.fft.fft(apodized)
+    return wavenumber, spectrum
+
+def loadInterferograms2D(filename, harm):
     regex = re.compile(r".*Interferometer.*\t(?P<center>(\d+\.\d+))\t(?P<distance>(\d+\.\d+)).*")
     distance = 0
     center = 0
@@ -49,45 +93,23 @@ def loadInterferograms(filename, harm):
             line = f.readline()
     data = pd.DataFrame(data).to_dict(orient="list")
     data = {x.replace(' ', ''): v for x, v in data.items()}
-    nbRun =int(data['Run'][-1]) + 1
-    runLength = int(len(data['Row'])/nbRun)
+    nbRun = int(data['Run'][-1]) + 1
+    nbRow = int(data['Row'][-1]) + 1
+    nbCol = int(data['Column'][-1]) + 1
+    runLength = int(len(data['Row'])/(nbRun*nbRow*nbCol))
     depth = np.asarray(data['Depth'],dtype=float)
     phase = np.asarray(data['O'+str(harm)+'P'],dtype=float)
     amp = np.asarray(data['O'+str(harm)+'A'],dtype=float)
     opticSig = amp * np.exp(1j*phase)
     
-    depth = depth.reshape([nbRun,runLength])
-    opticSig = opticSig.reshape([nbRun,runLength])
+    depth = depth.reshape([nbRow, nbCol, nbRun, runLength])
+    opticSig = opticSig.reshape([nbRow, nbCol, nbRun, runLength])
     
-    pos = depth * distance/len(depth[0]) + center-distance/2
+    pos = depth * distance/len(depth[0,0,0]) + center-distance/2
     
     return pos, opticSig
 
-def interArrayToSpectra(posIn,opticSig,discardPhase = True, discardDC = True, windowF = None, paddingFactor = 1, shiftMaxToZero = False):
-    pos = posIn[0,:]
-    step = pos[1]-pos[0]
-    av = np.mean(opticSig,0)
-    if discardPhase:
-        av *= np.exp(-j*(np.angle(np.mean(av))))
-    if discardDC:
-        av -= np.mean(av)
-    if windowF != None:
-        window = windowF(len(av))
-    else:
-        window = 1
-    apodized = av*window
-    apodized = np.concatenate((apodized,np.zeros(len(apodized)*(paddingFactor-1))))
-    if shiftMaxToZero:
-        apodized = np.roll(apodized, -np.argmax(np.abs(apodized)))
-    if discardPhase:
-        wavenumber = np.linspace(0,0.25e-2/step,(int(len(apodized)/2))+1)
-        spectrum = np.fft.rfft(np.real(apodized))
-    else:
-        wavenumber = np.linspace(0,0.5e-2/step,len(apodized))
-        spectrum = np.fft.fft(apodized)
-    return wavenumber, spectrum
+def loadSpectraFromInter2D(filename, harm, **kwargs):
+    pos, opticSig = loadInterferograms2D(filename, harm)
+    return interArrayToSpectra2D(pos,opticSig, **kwargs)
 
-
-def loadSpectraFromInter(filename, harm, **kwargs):
-    pos, opticSig = loadInterferograms(filename, harm)
-    return interArrayToSpectra(pos,opticSig, **kwargs)
